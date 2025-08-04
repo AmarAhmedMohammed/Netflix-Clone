78638a662c9e422fd5cd28caafd42c730b6468ab
Amar Ahmed <amarselmansudeys@gmail.com>
2025-08-04 21:00:14 +0300

    first Updat

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./css/bootstrap.css"
+import "./css/App.css";
+
+function App() {
+
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="">hello world!</h1>
+    </>
+  );
+}
+
+export default App;
